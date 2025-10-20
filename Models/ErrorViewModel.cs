6b394e51d5d71208c7322ca6975243555aa902a4
poorijak ratsamee <poorijak35@gmail.com>
2025-10-20 17:53:58 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Vex_E_commerce.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
