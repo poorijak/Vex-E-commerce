b3b136e69cb04ff1d25e41d4b935a09e73fee7aa
thirawat27892 <bboyloser@gamil.com>
2025-11-09 22:17:34 +0700

    create wishlist controller

---
+++ Controllers/WistlistController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vex_E_commerce.Controllers
+{
+    public class WistlistController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
