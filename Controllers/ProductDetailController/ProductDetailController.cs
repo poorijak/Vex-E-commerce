9b4be73923c9a2cb8fe90b616562402994f19d78
poorijak ratsamee <poorijak35@gmail.com>
2025-11-22 02:45:38 +0700

    fix : add fix totalCard and fix bug ui in cart page , Product Detail Page

--- Controllers/ProductDetailController/ProductDetailController.cs
+++ Controllers/ProductDetailController/ProductDetailController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using Vex_E_commerce.Data;
@@ -8,22 +9,53 @@ namespace Vex_E_commerce.Controllers
 {
     public class ProductDetailController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<Customer> _userManager;
 
-        public ProductDetailController(ApplicationDbContext context)
+        public ProductDetailController(ILogger<HomeController> logger, ApplicationDbContext context, UserManager<Customer> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Index(Guid? id)
         {
+            var userId = _userManager.GetUserId(User);
+
+            HashSet<Guid> wishedIds = new();
+            if (userId != null)
+            {
+                wishedIds = (await _context.ProductWishlists
+                    .AsNoTracking()
+                    .Where(w => w.UserId == userId)
+                    .Select(w => w.ProductId)
+                    .ToListAsync()).ToHashSet();
+            }
 
             if (id == null) return NotFound();
 
             var product = await _context.Products
-                .Include(p => p.ProductVariants)                              
+                .Include(p => p.ProductVariants)
                 .Include(p => p.Category)
                 .FirstOrDefaultAsync(p => p.Id == id);
+
+
+            var relateProduct = await _context.Products
+            .AsNoTracking()
+            .Where(p => p.Id != product.Id && p.Status == ProductStatus.Active)
+            .Select(p => new ProductCardVm
+            {
+            Id = p.Id,
+            Title = p.Title,
+            PictureUrl = p.PictureUrl,
+            TotalSold = p.TotalSold,
+            BasePrice = p.BasePrice,
+            IsWishlisted = wishedIds.Contains(p.Id)
+            })
+            .Take(5)
+            .ToListAsync();
+
 
             if (product == null)
                 return NotFound();
@@ -33,41 +65,26 @@ namespace Vex_E_commerce.Controllers
                        .ToDictionary(g => g.Key.ToString(), g => g.Sum(v => v.Stock));
 
             ViewBag.SizeStock = sizeStock;
+            ViewBag.categoryTitle = product.Category.Title;
 
 
             ViewBag.VariantsJson = System.Text.Json.JsonSerializer.Serialize(
-    product.ProductVariants.Select(v => new
-    {
-        color = v.Color.ToString(),
-        size = v.Size.ToString(),
-        stock = v.Stock
-    })
-);
+            product.ProductVariants.Select(v => new
+            {
+                color = v.Color.ToString(),
+                size = v.Size.ToString(),
+                stock = v.Stock
+            })
+            );
 
+            var vm = new ProductDetailDTO
+            {
+                Product = product,
+                RelateProducts = relateProduct
+            };
 
-
-            return View(product); 
+            return View(vm);
         }
-
-        //[HttpGet]
-        //public IActionResult GetVariantsBySize(Guid productId, string color, string size)
-        //{
-        //    ProductColor colorEnum = Enum.Parse<ProductColor>(color);
-        //    ProductSize sizeEnum = Enum.Parse<ProductSize>(size);
-
-        //    var variant = _context.ProductVariants
-        //        .Where(v => v.ProductId == productId
-        //                    && v.Color == colorEnum
-        //                    && v.Size == sizeEnum)
-        //        .FirstOrDefault(); // ตัวเดียว
-
-        //    return PartialView("_VariantListPartial", variant); // ส่งตัวเดียว
-        //}
-
-
-
-
-
 
 
 
